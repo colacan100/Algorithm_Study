2d7ca7d4e2764503b52f2ee712d9d3f7fb083921
colacan100 <85010611+colacan100@users.noreply.github.com>
2022-02-15 23:20:56 +0900

    baekjoon - implement

--- 2446.py
+++ 2446.py
@@ -2,6 +2,10 @@ import sys
 N = int(sys.stdin.readline())
 star = '*'
 blank = ' '
+count = 1+2*(N-1)
+for i in range(N-1):
+    print(blank*(i)+ star*count)
+    count -= 2
 count = 1
 for i in range(N):
     print(blank*(N-i-1)+ star*count)

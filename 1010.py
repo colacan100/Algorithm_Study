740e859194fb4142e0a4630627005ffbe49e3588
colacan100 <85010611+colacan100@users.noreply.github.com>
2022-03-16 23:20:03 +0900

    baekjoon - math

---
+++ 1010.py
@@ -0,0 +1,7 @@
+import sys
+import math
+T = int(sys.stdin.readline())
+for _ in range(T):
+    N,M= map(int,sys.stdin.readline().split())
+    bridge = math.factorial(M) // (math.factorial(N) * math.factorial(M - N))
+    print(bridge)

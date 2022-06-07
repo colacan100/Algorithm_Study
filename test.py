a3ee7c3f8fb3a9581f2dffc46d1ee3ef3bc3ce8a
colacan100 <85010611+colacan100@users.noreply.github.com>
2022-06-07 17:37:08 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,33 @@
-A, B = map(int,input().split())
+# 캡슐화코드
+
+class Encap:
+  def __init__(self,value):
+    self.value = value
+    print('init :', self.value)
+
+  def _set(self):
+    print('set :', self.value)
+
+  def printTest(self):
+    print('printTest :', self.value)
+
+  # def __printTest2(self):
+  #   print('printTest :', self.value)
+
+# object 생성
+e = Encap(10)
+
+# object 실행 
+# 케이스1
+e.__init__(20)
+e._set()
+e.printTest()
+#e.__printTest2()
+
+
+print('\n')
+
+# 케이스2
+e.__init__(30)
+e._set()
+e.printTest()
